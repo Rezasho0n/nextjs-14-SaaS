4c5d54f643f264c3be7cbc68ec38f6502b1e5dd3
Reza <reza.vataninezafat@gmail.com>
2024-07-02 17:57:28 -0400

    add api crud

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -2,6 +2,11 @@ import About from "@/components/About";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import Team from "@/components/Team";
 import { Metadata } from "next";
+import { generateClient } from "aws-amplify/api";
+import { createTodo } from './graphql/mutations';
+import { listTodos, getTodo } from "./graphql/queries";
+
+const client = generateClient()
 
 export const metadata: Metadata = {
   title:
@@ -18,5 +23,26 @@ const AboutPage = () => {
     </main>
   );
 };
+
+
+const newTodo = await client.graphql({
+  query: createTodo,
+  variables: {
+      input: {
+  "name": "Lorem ipsum dolor sit amet",
+  "description": "Lorem ipsum dolor sit amet"
+}
+  }
+});
+
+
+
+// List all items
+const allTodos = await client.graphql({
+  query: listTodos
+});
+console.log(allTodo);
+
+
 
 export default AboutPage;
