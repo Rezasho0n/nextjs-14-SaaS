c7985e3dd2ab2ab41c28f471945fa9cbb0893be7
Reza <reza.vataninezafat@gmail.com>
2024-07-03 12:16:13 -0400

    remove nextauth

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,34 +1,47 @@
-import bcrypt from "bcrypt";
 import { NextResponse } from "next/server";
-import { prisma } from "@/utils/prismaDB";
+import { Amplify } from 'aws-amplify';
+import { signUp } from 'aws-amplify/auth';
+import config from '@/aws-exports';
+import { AuthError } from '@aws-amplify/auth';
 
-export async function POST(request: any) {
-  const body = await request.json();
-  const { name, email, password } = body;
+Amplify.configure(config, { ssr: true });
 
-  if (!name || !email || !password) {
-    return NextResponse.json("Missing Fields", { status: 400 });
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { name, email, password } = body;
+
+    if (!name || !email || !password) {
+      return NextResponse.json({ error: "Missing Fields" }, { status: 400 });
+    }
+
+    const { isSignUpComplete, userId } = await signUp({
+      username: email,
+      password,
+      options: {
+        userAttributes: {
+          name,
+          email,
+        },
+      },
+    });
+
+    return NextResponse.json({ 
+      message: isSignUpComplete ? "User registered successfully" : "Further action required",
+      userId 
+    }, { status: 200 });
+  } catch (error: unknown) {
+    console.error('Error during registration:', error);
+    
+    if (error instanceof AuthError) {
+      if (error.name === 'UsernameExistsException') {
+        return NextResponse.json({ error: "User already exists" }, { status: 409 });
+      }
+      // Handle other specific AuthError types if needed
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+    
+    // For any other types of errors
+    return NextResponse.json({ error: "Registration failed" }, { status: 500 });
   }
-
-  const exist = await prisma.user.findUnique({
-    where: {
-      email: email.toLowerCase(),
-    },
-  });
-
-  if (exist) {
-    return NextResponse.json("User already exists!", { status: 500 });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  await prisma.user.create({
-    data: {
-      name,
-      email: email.toLowerCase(),
-      password: hashedPassword,
-    },
-  });
-
-  return NextResponse.json("User created successfully!", { status: 200 });
 }
