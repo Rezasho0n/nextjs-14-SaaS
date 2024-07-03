0b39bdf847f9e86a0ff2100de3748b9485c35a21
Reza <reza.vataninezafat@gmail.com>
2024-07-03 17:47:47 -0400

    remove next-auth from the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
@@ -32,7 +31,6 @@ export default function RootLayout({
           <PreLoader />
         ) : (
           <AuthProvider>
-            <SessionProvider>
               <ThemeProvider
                 attribute="class"
                 enableSystem={false}
@@ -44,7 +42,6 @@ export default function RootLayout({
                 <Footer />
                 <ScrollToTop />
               </ThemeProvider>
-            </SessionProvider>
           </AuthProvider>
         )}
       </body>
